5bd53a011a49a533ae4bff055d413cb736af12fb
Rafael Faust Rodrigues <106990982+rafaelfaust@users.noreply.github.com>
2025-05-28 22:55:36 -0300

    refactor: atualizar estilos globais e modificar a página inicial para usar um botão

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <Button>BootCamp!</Button>;
 };
 
 export default HomePage;
